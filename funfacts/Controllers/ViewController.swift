c0e8f901dd157e06988814e193b31c67fdbc8375
Derick Derick <DerickDerick@Dericks-MacBook-Air.local>
2017-12-07 21:33:47 +0800

    Initial Commit

---
+++ funfacts/Controllers/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Derick Junior on 31/08/2017.
+//  Copyright © 2017 Indiemax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
+    
+    let factProvider = FactProvider()
+    let colorProvider = BackgroundColorProvider()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        funFactLabel.text = factProvider.randomFact()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showFact() {
+        funFactLabel.text = factProvider.randomFact()
+        let randomColor = colorProvider.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+    }
+
+}
+
